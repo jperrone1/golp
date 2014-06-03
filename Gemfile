380627bee8bdbf874defeb63595191424f58ff56
Jeffrey Perrone <jperrone@well.com>
2014-06-02 18:29:56 -0700

    Reverting to rspec-rails 2.99

--- Gemfile
+++ Gemfile
@@ -37,7 +37,8 @@ group :development do
 end
 
 group :development, :test do 
-  gem 'rspec-rails'
+  gem 'rspec-rails', '2.99'
+  gem 'capybara'
 end
 
 
---
+++ spec/features/hello_world_spec.rb
@@ -0,0 +1,10 @@
+# hello_world_spec.rb
+
+require 'spec_helper'
+
+feature 'User wants a greeting' do
+  scenario 'says Hello World' do
+    visit helloworld_path
+    expect(page).to have_css 'h1', :text => 'Hello World', :visible => true
+  end
+end
